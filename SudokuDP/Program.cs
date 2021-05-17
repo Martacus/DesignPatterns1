a5561f6b458e98536ba144e72e62fd68a4c72b1e
Mart <mcpkmart@hotmail.nl>
2021-05-17 14:23:04 +0200

    First push

---
+++ SudokuDP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SudokuDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
