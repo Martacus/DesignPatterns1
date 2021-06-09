7e44a2e024c27e88c5980cc94573b0475b8649d4
Mart <mcpkmart@hotmail.nl>
2021-06-09 13:27:32 +0200

    Factory works for all types of normal sudoku

--- SudokuDP/Factory/SudokuFactory.cs
+++ SudokuDP/Factory/SudokuFactory.cs
@@ -26,13 +26,13 @@ namespace SudokuDP.Factory
                 board.Cells[i] = new Cell(cellNumber);
             }
 
-            
+
             for (int i = 0; i < columns; i++)
             {
                 ICell row = new Row(columns);
-                for (int j = 0; j < 9; j++)
+                for (int j = 0; j < columns; j++)
                 {
-                    row.children[j] = board.Cells[(i * 9) + j];
+                    row.children[j] = board.Cells[(i * columns) + j];
                 }
                 board.Rows[i] = row;
             }
@@ -40,42 +40,63 @@ namespace SudokuDP.Factory
             for (int i = 0; i < columns; i++)
             {
                 ICell row = new Row(columns);
-                for (int j = 0; j < 9; j++)
-                {
-                    row.children[j] = board.Cells[(j * 9) + i];
-                }
-                board.Rows[9 + i] = row;
-            }
-
-            int columnOffset = 0;
-            for (int i = 0; i < columns; i++)
-            {
-                ICell column = new Column(columns);
-                int next = 0;
                 for (int j = 0; j < columns; j++)
                 {
-                    
-                    //column.children[j] = board.Cells[(next * 9) + i];
-                    Console.WriteLine((next * 9) + j + columnOffset + (i / 3 * 27));
-                    if ((j + 1) % 3 == 0)
-                    {
-                        next++;
-                    }
+                    row.children[j] = board.Cells[(j * columns) + i];
                 }
-                board.Columns[i] = column;
-                columnOffset += 3;
-
-                if (columnOffset == 9)
-                {
-                    columnOffset = 0;
-                }
+                board.Rows[columns + i] = row;
             }
 
-            board.drawBoard();
+            Dictionary<int, int[]> offsets = new Dictionary<int, int[]>();
+            offsets.Add(9, new int[] { 3, 3, 3 });
+            //0,
+            //27,
+            //54
+            //    81 / 3
+            offsets.Add(6, new int[] { 3, 2, 3 });
+            //0, 
+            //12,
+            //24
+            //    36 / 3
+            offsets.Add(4, new int[] { 2, 2, 2 });
+            //0, 8, 
+            //16 / 2
 
-            Console.ReadLine();
+            board.Columns = new ICell[columns];
+            int filledColumns = 0;
+            int columnRowOffset = 0;
+            for (int i = 0; i < offsets[columns][0]; i++)
+            {
+                var offsetOne = offsets[columns][0];
+                var offsetTwo = offsets[columns][1];
+                var offsetThree = offsets[columns][2];
+                int columnOffset = 0;
+                for (int j = 0; j < offsetTwo; j++)
+                {
+                    
+                    ICell column = new Column(columns);
+                    for (int k = 0; k < offsetThree; k++)
+                    {
+                        for (int p = 0; p < offsetThree; p++)
+                        {
+                            column.children[k * offsetThree + p] = board.Cells[p + (k * columns) + columnOffset + columnRowOffset];
+                        }
+                    }
+                    columnOffset = columnOffset + offsetOne;
+                    board.Columns[filledColumns] = column;
+                    filledColumns++;
+                }
+                columnRowOffset += board.Cells.Length / offsetOne;
+            }
 
-            throw new NotImplementedException();
+            return board;
+
         }
+    
+           
+
+
+          
+        
     }
 }
